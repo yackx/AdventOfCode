95a1a94f68ef8965490f4b027a26f01fa1dfb0e6
Youri Ackx <youri@ackx.net>
2016-12-04 11:45:57 +0100

    2016 day 4 part 1 cleanup and comment

--- 2016/src/groovy/day04-part1.groovy
+++ 2016/src/groovy/day04-part1.groovy
@@ -7,16 +7,17 @@ def input = new File('day04.txt') as String[]
 input.each { line ->
     def (_, room, sector, checksum) = (line =~ re)[0]
     room = room.replaceAll('-', '')
-    println "$room $sector $checksum"
-    def letters = room.toList().unique()
-    println letters
-    def m = letters.collectEntries { [(it): room.count(it)] }
-    println m
-    def foo = m.sort { a, b -> b.value <=> a.value ?: a.key <=> b.key }
-    def bar = foo.take(5).keySet().join()
-    println bar
-    if (bar == checksum) sum += sector as int
-    println()
+    def digest = room               // 'aaaaa-bbb-z-y-x'
+            .toList()               // ['a', 'a', ... 'y', 'x'
+            .unique()               // ['a', 'b', 'z', 'y', 'x']
+                                    // ['a': 5, 'b': 3, ...]
+            .collectEntries { [(it): room.count(it)] }
+                                    // ['a': 5, 'b': 3, 'x': 1, ...]
+            .sort { a, b -> b.value <=> a.value ?: a.key <=> b.key }
+            .take(5)
+            .keySet()               // ['a', 'b', 'x', 'y', 'z']
+            .join()                 // 'abxyz'
+    if (digest == checksum) sum += sector as int
 }
 
 println sum
