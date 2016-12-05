e9c6a0b945b565c85138706aaf0ab754a64793a7
Youri Ackx <youri@ackx.net>
2016-12-05 16:15:28 +0100

    Even shorter by using count()

--- 2016/src/groovy/day03-part1.groovy
+++ 2016/src/groovy/day03-part1.groovy
@@ -2,11 +2,7 @@
 
 def input = new File('day03.txt') as String[]
 
-int possible = 0
-
-input.each { dims ->
+println input.count { dims ->
     def (a, b, c) = dims.split()*.toInteger()
-    if (a + b > c && a + c > b && b + c > a) possible++
+    (a + b > c && a + c > b && b + c > a)
 }
-
-println possible
