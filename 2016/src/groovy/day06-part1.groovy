30b76e89c0ae9e023d2f6a272d3e98e883158b11
Youri Ackx <youri@ackx.net>
2016-12-06 22:00:54 +0100

    Removed return

--- 2016/src/groovy/day06-part1.groovy
+++ 2016/src/groovy/day06-part1.groovy
@@ -4,7 +4,7 @@ def input = new File('day06.txt') as String[]
 
 def chars = (0..input[0].length()-1).collect({ n ->
     def row = input*.charAt(n)
-    return row.collectEntries {[ (it): row.count(it) ]}.max {e -> e.value}.key
+    row.collectEntries {[ (it): row.count(it) ]}.max {e -> e.value}.key
 })
 
 println chars.join()
