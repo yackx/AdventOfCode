e94d79f3d38c54635005edb2d0978cea8736bb0d
Youri Ackx <youri@ackx.net>
2017-12-01 14:35:59 +0100

    2017 day 01 kotlin - no need for toCharArray()

--- 2017/src/day01/part01.kt
+++ 2017/src/day01/part01.kt
@@ -3,9 +3,9 @@ package day01
 import java.io.File
 
 fun main(args: Array<String>) {
-    val input = File("day01/input.txt").bufferedReader().use { it.readText() }.toCharArray()
+    val input = File("day01/input.txt").bufferedReader().use { it.readText() }
     val sum = input.indices
-            .filter { input[it] == input[(it+1)%input.size] }
+            .filter { input[it] == input[(it+1) % input.length] }
             .sumBy { input[it].toInt() - '0'.toInt() }
     println(sum)
 }
--- 2017/src/day01/part02.kt
+++ 2017/src/day01/part02.kt
@@ -3,10 +3,10 @@ package day01
 import java.io.File
 
 fun main(args: Array<String>) {
-    val input = File("day01/input.txt").bufferedReader().use { it.readText() }.toCharArray()
-    val size = input.size
+    val input = File("day01/input.txt").bufferedReader().use { it.readText() }
+    val size = input.length
     val sum = input.indices
-            .filter { input[it] == input[(it+size/2)%size] }
+            .filter { input[it] == input[(it+size/2) % size] }
             .sumBy { input[it].toInt() - '0'.toInt() }
     println(sum)
 }
