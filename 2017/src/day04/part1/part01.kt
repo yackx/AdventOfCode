5e6abebf727408810c82cf922bf79b491130690a
Youri Ackx <youri@ackx.net>
2017-12-04 09:53:22 +0100

    2017 day 04 kotlin - part 1

---
+++ 2017/src/day04/part1/part01.kt
@@ -0,0 +1,13 @@
+package day04.part1
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val count = File("day04/part1/input.txt").bufferedReader().lines()
+            .filter {
+                val splitted = it.split(" ")
+                splitted.size == splitted.toSet().size
+            }
+           .count()
+    println(count)
+}
