8dc29800fd96b54ccd3780220e39a83aec5b2190
Youri Ackx <youri@ackx.net>
2017-12-04 11:24:41 +0100

    2017 day 04 kotlin - part 1 - adjust input path

--- 2017/src/day04/part1/part01.kt
+++ 2017/src/day04/part1/part01.kt
@@ -3,7 +3,7 @@ package day04.part1
 import java.io.File
 
 fun main(args: Array<String>) {
-    val count = File("day04/part1/input.txt").bufferedReader().lines()
+    val count = File("day04/input.txt").bufferedReader().lines()
             .filter {
                 val splitted = it.split(" ")
                 splitted.size == splitted.toSet().size
