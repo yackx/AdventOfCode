be5e36bc94f4810dc889cbcebec4a297f6113184
Youri Ackx <youri@ackx.net>
2017-12-06 10:14:34 +0100

    2017 day 05 kotlin - improve part 2:
    * More elegant and kotlinic `for` expression with indices
    * Store the next index in tempo variable, to avoid storing old index
      + old value
    
    Thanks @edarchis

--- 2017/src/day05/part02.kt
+++ 2017/src/day05/part02.kt
@@ -6,11 +6,10 @@ fun main(args: Array<String>) {
     val offsets = File("day05/input.txt").bufferedReader().readLines().map { it.toInt() }.toMutableList()
     var steps = 0
     var index = 0
-    while (index >= 0 && index < offsets.size) {
-        val prevIndex = index
-        val prevOffset = offsets[index]
-        index += offsets[index]
-        offsets[prevIndex] += if (prevOffset >= 3) - 1 else 1
+    while (index in offsets.indices) {
+        val nextIndex = index + offsets[index]
+        offsets[index] += if (offsets[index] >= 3) - 1 else 1
+        index = nextIndex
         steps++
     }
     println(steps)
