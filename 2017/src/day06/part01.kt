4f3c64e14cb4497595a61a006a48490509714169
Youri Ackx <youri@ackx.net>
2017-12-06 15:02:15 +0100

    2017 day 06 part 1 kotlin

---
+++ 2017/src/day06/part01.kt
@@ -0,0 +1,21 @@
+package day06
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val banks = File("day06/input.txt").bufferedReader().readLine()!!.split("\t").map { it.toInt() }.toMutableList()
+    val seen = mutableListOf<List<Int>>()
+
+    while (true) {
+        val index = banks.indices.maxBy { idx -> banks[idx] }!!
+        val value = banks[index]
+        banks[index] = 0
+        for (i in 1..value) {
+            banks[(index+i) % banks.size]++
+        }
+        if (banks in seen) break
+        seen.add(banks.toList())
+    }
+
+    println(seen.size + 1)
+}
