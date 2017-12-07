e8e682558d3ef341d944bb06105732b6c8656a94
Youri Ackx <youri@ackx.net>
2017-12-07 10:00:45 +0100

    2017 day 04 part 1 kotlin much improved
    Based on a suggestion by @edarchis who used Map.associateBy(),
    this is an even shorter version using a sorted set.

--- 2017/src/day04/part2/part02.kt
+++ 2017/src/day04/part2/part02.kt
@@ -2,20 +2,11 @@ package day04.part2
 
 import java.io.File
 
-private fun areAnagrams(s1: String, s2: String) = s1.toSortedSet() == s2.toSortedSet()
-
 fun main(args: Array<String>) {
     val count = File("day04/input.txt").bufferedReader().lines()
             .filter { line ->
-                val split = line.split(" ")
-                var anagramFound = false
-                for ((index, word) in split.withIndex()) {
-                    anagramFound = split.toList()
-                            .filterIndexed { idx, _ -> idx != index }
-                            .any { areAnagrams(it, word) }
-                    if (anagramFound) break
-                }
-                !anagramFound
+                val words = line.split(" ")
+                words.map { it.toSortedSet() }.distinct().size == words.size
             }
             .count()
     println(count)
