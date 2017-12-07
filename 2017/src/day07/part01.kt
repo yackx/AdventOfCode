48564061858fb8cec6c8480a31925b58bb2a2a44
Youri Ackx <youri@ackx.net>
2017-12-07 16:23:39 +0100

    2017 day 07 part 1 - using destructured

--- 2017/src/day07/part01.kt
+++ 2017/src/day07/part01.kt
@@ -6,17 +6,16 @@ fun main(args: Array<String>) {
     val notRoot = mutableSetOf<String>()
     val couldBeRoot = mutableSetOf<String>()
     val re = Regex("(.*) \\(.*\\)( -> )?(.*)?")
-    File("day07/input.txt").bufferedReader().lines().forEach { line ->
-        val children = re.matchEntire(line)!!.groupValues[3].split(", ")
-        println("CHILDREN $children")
-        notRoot.addAll(children)
-        couldBeRoot.removeAll(children)
-        val program = re.matchEntire(line)!!.groupValues[1]
-        if (program !in notRoot) couldBeRoot.add(program)
-        println(line)
-        println("Could be root: $couldBeRoot")
-        println("notRoot: $notRoot")
-        println()
+    File("day07/inputf.txt").bufferedReader().lines().forEach { line ->
+        val (candidate, _, childrenList, _) = re.matchEntire(line)!!.destructured
+        if (!childrenList.isEmpty()) {
+            val children = childrenList.split(", ")
+            notRoot.addAll(children)
+            couldBeRoot.removeAll(children)
+        }
+        if (candidate !in notRoot) {
+            couldBeRoot.add(candidate)
+        }
     }
     if (couldBeRoot.size != 1) throw IllegalStateException("root candidates: $couldBeRoot")
     println(couldBeRoot.first())
