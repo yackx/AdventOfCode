1d35cdacc2425ba1a85b93d2e712c69aa6d06f7e
Youri Ackx <youri@ackx.net>
2017-12-09 14:26:52 +0100

    2017 day 09 part 1

---
+++ 2017/src/day09/part01.kt
@@ -0,0 +1,43 @@
+package day09
+
+import java.io.File
+
+fun score(s: String): Int {
+    var score = 0
+    var level = 0
+    var ignoreNext = false
+    var inGarbage = false
+    for (c in s) {
+        if (ignoreNext) {
+            ignoreNext = false
+            continue
+        }
+        when (c) {
+            '{' -> if (!inGarbage) {
+                    level++
+                    score += level
+                }
+            '}' -> if (!inGarbage) level--
+            '!' -> ignoreNext = true
+            '<' -> inGarbage = true
+            '>' -> inGarbage = false
+        }
+    }
+    return score
+}
+
+fun main(args: Array<String>) {
+    val input = File("day09/input.txt").readLines().first()
+    println(score(input))
+}
+
+fun eaTests() {
+    assert(score("{}") == 1)
+    assert(score("{{{}}}") == 6)
+    assert(score("{{},{}}") == 5)
+    assert(score("{{{},{},{{}}}}") == 16)
+    assert(score("{<a>,<a>,<a>,<a>}") == 1)
+    assert(score("{{<ab>},{<ab>},{<ab>},{<ab>}}") == 9)
+    assert(score("{{<!!>},{<!!>},{<!!>},{<!!>}}") == 9)
+    assert(score("{{<a!>},{<a!>},{<a!>},{<ab>}}") == 3)
+}
