899dceed4bd63582969bdee1c066d9edd7a036ed
Youri Ackx <youri@ackx.net>
2018-06-25 13:56:41 +0200

    Rename var

--- 2017/src/day02/part02a.kt
+++ 2017/src/day02/part02a.kt
@@ -7,7 +7,7 @@ import java.io.File
 fun main(args: Array<String>) {
     val checksum = File("src/day02/input.txt").readLines().sumBy { line ->
         val values = line.split("\t").map { it.toInt() }
-        val f = fun(): Int {
+        val v = fun(): Int {
             for (x in values) {
                 for (y in values) {
                     if (x != y && x % y == 0) {
@@ -17,7 +17,7 @@ fun main(args: Array<String>) {
             }
             throw IllegalStateException(values.toString())
         }()
-        f
+        v
     }
     println(checksum)
 }
