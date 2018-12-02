ab6e3aed9ceb288c9b23c39728378c96882ea1c7
Youri Ackx <youri@ackx.net>
2018-12-02 14:15:07 +0100

    Improve code readability

--- 2018/day02/part1.py
+++ 2018/day02/part1.py
@@ -2,15 +2,14 @@
 
 from collections import Counter
 
-with open('input.txt') as f:
-    data = f.read().splitlines()
+with open('input.txt') as file:
+    data = file.read().splitlines()
 
 two = 0
 three = 0
 for box in data:
-    freq = Counter(box)
-    if len([f for f in freq.values() if f == 2]) > 0:
-        two = two + 1
-    if len([f for f in freq.values() if f == 3]) > 0:
-        three = three + 1
-print(two*three)
+    frequencies = Counter(box)
+    counter = lambda n: len([f for f in frequencies.values() if f == n])
+    two = two + 1 if counter(2) else two
+    three = three + 1 if counter(3) else three
+print(two * three)
