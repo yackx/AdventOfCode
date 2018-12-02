647e8506cde8994c221f87a248a474a1be7a279c
Youri Ackx <youri@ackx.net>
2018-12-02 15:12:55 +0100

    Improve code readability by using a tuple

--- 2018/day02/part2.py
+++ 2018/day02/part2.py
@@ -7,8 +7,8 @@ with open('input.txt') as file:
 
 pairs = [p for p in list(product(data, data)) if p[0] < p[1]]
 
-for pair in pairs:
-    common = [pair[0][x] for x in range(len(pair[0])) if pair[0][x] == pair[1][x]]
-    if len(common) == len(pair[0]) - 1:
+for p1, p2 in pairs:
+    common = [p1[x] for x in range(len(p1)) if p1[x] == p2[x]]
+    if len(common) == len(p1) - 1:
         print(''.join(common))
         break
