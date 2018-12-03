b18d1388f038d964a04e06d40143ee98d01343ee
Youri Ackx <youri@ackx.net>
2018-12-03 21:00:33 +0100

    Declare lambda just before it is needed

--- 2018/day03/part2.py
+++ 2018/day03/part2.py
@@ -2,7 +2,7 @@
 
 import re
 
-patches = lambda c: set([(i, j) for i in range(c[1], c[1]+c[3]) for j in range(c[2], c[2]+c[4])])
 data = open('input.txt').read().splitlines()
 claims = [list(map(int, re.findall(r"\d+", line))) for line in data]
+patches = lambda c: set([(i, j) for i in range(c[1], c[1]+c[3]) for j in range(c[2], c[2]+c[4])])
 print([c[0] for c in claims if all(len(patches(k) & patches(c)) is 0 for k in claims if c[0] is not k[0])])
