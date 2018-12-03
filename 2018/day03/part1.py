e2e30f72e33fa610810e9453a95cbe53dc05c772
Youri Ackx <youri@ackx.net>
2018-12-03 20:57:40 +0100

    Slightly better

--- 2018/day03/part1.py
+++ 2018/day03/part1.py
@@ -7,8 +7,7 @@ data = open('input.txt').read().splitlines()
 claims = [list(map(int, re.findall(r"\d+", line))) for line in data]
 for claim in claims:
     _, left, top, width, height = claim
-    p = lambda l, t, w, h: [(i, j) for i in range(l, l+w) for j in range(t, t+h)]
-    for patch in p(left, top, width, height):
+    for patch in [(i, j) for i in range(left, left+width) for j in range(top, top+height)]:
         patches[patch] = patches.get(patch, 0) + 1
 
 print(len([n for n in patches.values() if n > 1]))
