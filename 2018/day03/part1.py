2584cea01b3b69593941da23ebe989d74d79703d
Youri Ackx <youri@ackx.net>
2018-12-03 20:52:54 +0100

    Load file in memory

--- 2018/day03/part1.py
+++ 2018/day03/part1.py
@@ -3,13 +3,13 @@
 import re
 
 patches = dict()
-with open('input.txt') as f:
-    for line in f:
-        values = map(int, re.findall(r"\d+", line))
-        _, left, top, width, height = values
-        for i in range(left, left+width):
-            for j in range(top, top+height):
-                patch = (i, j)
-                patches[patch] = patches.get(patch, 0) + 1
+data = open('input.txt').read().splitlines()
+claims = [list(map(int, re.findall(r"\d+", line))) for line in data]
+for claim in claims:
+    _, left, top, width, height = claim
+    for i in range(left, left+width):
+        for j in range(top, top+height):
+            patch = (i, j)
+            patches[patch] = patches.get(patch, 0) + 1
 
 print(len([n for n in patches.values() if n > 1]))
