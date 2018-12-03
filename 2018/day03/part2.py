492c10234245a96adb424feaf708b7bbff6ba8ee
Youri Ackx <youri@ackx.net>
2018-12-03 21:02:22 +0100

    Use a generator. No obvious gain however

--- 2018/day03/part2.py
+++ 2018/day03/part2.py
@@ -5,4 +5,4 @@ import re
 data = open('input.txt').read().splitlines()
 claims = [list(map(int, re.findall(r"\d+", line))) for line in data]
 patches = lambda c: set([(i, j) for i in range(c[1], c[1]+c[3]) for j in range(c[2], c[2]+c[4])])
-print([c[0] for c in claims if all(len(patches(k) & patches(c)) is 0 for k in claims if c[0] is not k[0])])
+print(next(c[0] for c in claims if all(len(patches(k) & patches(c)) is 0 for k in claims if c[0] is not k[0])))
