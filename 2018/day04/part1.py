d1e7d66166e2e6f849ddbfcdfc88ae8cd1226373
Youri Ackx <youri@ackx.net>
2018-12-04 12:59:17 +0100

    Add day04 part 2

--- 2018/day04/part1.py
+++ 2018/day04/part1.py
@@ -3,37 +3,58 @@
 import re
 from collections import Counter
 
-data = sorted(open('input.txt').read().splitlines())
 
-minutes_per_guard = dict()              # k=guard_id, v=[all minutes asleep]
-current_guard = None
-start = None
-finish = None
-r_cycle = lambda s: rf".*:(\d\d).*{s}"  # falls or wakes
+def minutes_per_guard():
+    data = sorted(open('input.txt').read().splitlines())
 
-for line in data:
-    guard = re.findall(r".*#(\d+).*", line)
-    if guard:
-        current_guard = int(guard[0])
+    mpg = dict()              # k=guard_id, v=[all minutes asleep]
+    current_guard = None
+    start = None
+    r_cycle = lambda s: rf".*:(\d\d).*{s}"  # falls or wakes
 
-    falls = re.findall(r_cycle('falls'), line)
-    if falls:
-        start = int(falls[0])
+    for line in data:
+        guard = re.findall(r".*#(\d+).*", line)
+        if guard:
+            current_guard = int(guard[0])
 
-    wakes = re.findall(r_cycle('wakes'), line)
-    if wakes:
-        finish = int(wakes[0])
-        new_mins = list(range(start, finish))
-        current_mins = minutes_per_guard.get(current_guard, [])
-        minutes_per_guard[current_guard] = current_mins + new_mins
+        falls = re.findall(r_cycle('falls'), line)
+        if falls:
+            start = int(falls[0])
 
-# k = guard id, v = total number of minutes asleep
-count_per_guard = {k: len(v) for (k, v) in minutes_per_guard.items()}
-# sleeper id = max number of minutes asleep
-sleeper_id = max(count_per_guard, key=count_per_guard.get)
-# k = minute, v = how many times
-frequencies = Counter(minutes_per_guard[sleeper_id])
-# minute where guard was mostly asleep
-minute = max(frequencies, key=frequencies.get)
+        wakes = re.findall(r_cycle('wakes'), line)
+        if wakes:
+            finish = int(wakes[0])
+            new_mins = list(range(start, finish))
+            current_mins = mpg.get(current_guard, [])
+            mpg[current_guard] = current_mins + new_mins
 
-print(sleeper_id * minute)
+    return mpg
+
+
+def part1(minutes_per_guard):
+    # k = guard id, v = total number of minutes asleep
+    count_per_guard = {k: len(v) for (k, v) in minutes_per_guard.items()}
+    # sleeper with max number of minutes asleep
+    sleeper_id = max(count_per_guard, key=count_per_guard.get)
+    # k = minute, v = how many times
+    frequencies = Counter(minutes_per_guard[sleeper_id])
+    # minute where guard was mostly asleep
+    minute = max(frequencies, key=frequencies.get)
+
+    return sleeper_id * minute
+
+
+def part2(minutes_per_guard):
+    # eg: {10: (24, 2), 99: (45, 3)}
+    most_commons = {k: Counter(v).most_common(1)[0] for (k, v) in minutes_per_guard.items()}
+    # eg: 99 (because 45 appears 3 times)
+    sleeper_id = max(most_commons, key=lambda k: most_commons[k][1])
+    # eg: 45
+    minute = most_commons[sleeper_id][0]
+
+    return sleeper_id * minute
+
+
+mpg = minutes_per_guard()
+print(part1(mpg))
+print(part2(mpg))
