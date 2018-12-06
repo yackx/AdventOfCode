75f1334bb8d1d23a9becefeb20facaec225b3685
Youri Ackx <youri@ackx.net>
2018-12-06 13:03:48 +0100

    Compact solution

--- 2018/day06/part2.py
+++ 2018/day06/part2.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
 import re
-from collections import Counter
+
+safe = 10000
 
 
 def distance(c1, c2):
     return abs(c1[0] - c2[0]) + abs(c1[1] - c2[1])
 
 
-safe = 10000
+def is_safe_spot(p):
+    sum_of_distances = sum([distance(p, c) for c in coords])
+    return sum_of_distances < safe
+
 
 data = open('input.txt').read().splitlines()
 coords = [(x, y) for (x, y) in (map(int, re.findall(r"\d+", line)) for line in data)]
@@ -19,12 +23,6 @@ right = max(n[0] for n in coords)
 top = min(n[1] for n in coords)
 bottom = max(n[1] for n in coords)
 
-count = 0
-for x in range(left, right):
-    for y in range(top, bottom):
-        p = (x, y)
-        sum_of_distances = sum([distance(p, c) for c in coords])
-        if sum_of_distances < safe:
-            count += 1
 
-print(count)
+locations = ((x, y) for x in range(left, right) for y in range(top, bottom))
+print(len([p for p in locations if is_safe_spot(p)]))
