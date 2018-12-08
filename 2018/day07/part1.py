287a8f049482f37f563ca349fed9524e8cc1a370
Youri Ackx <youri@ackx.net>
2018-12-08 10:17:03 +0100

    Simplify solution. Use tuples only for instructions

--- 2018/day07/part1.py
+++ 2018/day07/part1.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
 import re
-from collections import defaultdict
 
-instructions = open('input.txt').read().splitlines()
-predecessors = defaultdict(lambda: set())
-for instruction in instructions:
-    pre, step = re.findall(r'Step ([A-Z]).*([A-Z])', instruction)[0]
-    predecessors[step].add(pre)
-    if pre not in predecessors.keys():
-        predecessors[pre] = set()
+lines = open('input.txt').read().splitlines()
+instructions = [re.findall(r'Step ([A-Z]).*([A-Z])', i)[0] for i in lines]
+print(instructions)
+
 
 done = []
-while predecessors:
-    no_predecessor = sorted(k for (k, v) in predecessors.items() if len(v) == 0)[0]
-    done.append(no_predecessor)
-    predecessors.pop(no_predecessor)
-    predecessors = {k: v-set(no_predecessor) for (k, v) in predecessors.items()}
+while instructions:
+    if len(instructions) == 1:  # TODO Improve this unfortunate corner case
+        a, b = instructions[0]
+        done.append(a)
+        done.append(b)
+    steps_with_pred = [v for k, v in instructions]
+    print('IN ', instructions)
+    step = sorted([k for k, v in instructions if k not in steps_with_pred])[0]
+    done.append(step)
+    print('     ',steps_with_pred, step)
+    instructions = [(k, v) for k, v in instructions if k is not done[-1]]
+    print('OUT', instructions, done, step)
 
 print(''.join(done))
