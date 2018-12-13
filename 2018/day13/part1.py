a0609397a83e79ead63f2c6d411d83adc3e301c9
Youri Ackx <youri@ackx.net>
2018-12-13 17:06:36 +0100

    Add day13 part 1 (inverted x and y)

---
+++ 2018/day13/part1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import operator
+
+data = open('sample.txt').read().splitlines()
+# Build a grid of rails and carts: {(0, 0): '/', (0, 1): '-', ...}
+grid = {(i, j): data[i][j] for i in range(len(data)) for j in range(len(data[i]))}
+# Filter out blanks
+grid = {k: v for k, v in grid.items() if v is not ' '}
+# Extract carts: {(0, 2): '>', (3, 9): 'v'}
+cart_symbols = ['<', '>', 'v', '^']
+carts = {k: v for k, v in grid.items() if v in cart_symbols}
+# Define a left next turn for each cart: {(0, 2): ('>', '<'), (3, 9): ('v', '<')}
+carts = {k: (v, 'left') for k, v in carts.items()}
+# Turn carts into '-' rails on the original grid
+rails = {k: v if v not in cart_symbols else '-' for k, v in grid.items()}
+
+cart_directions = {
+    '<': (0, -1), '>': (0, +1), 'v': (+1, 0), '^': (-1, 0),
+}
+rail_directions = {
+    ('/', '^'): '>', ('/', '<'): 'v', ('/', '>'): '^', ('/', 'v'): '<',
+    ('\\', '^'): '<', ('\\', '>'): 'v', ('\\', '<'): '^', ('\\', 'v'): '>',
+}
+turns = {
+    # (turn, current_direction), new_direction
+    ('left', '^'): '<', ('left', 'v'): '>', ('left', '>'): '^', ('left', '<'): 'v',
+    ('straight', '<'): '<', ('straight', '>'): '>', ('straight', '^'): '^', ('straight', 'v'): 'v',
+    ('right', '^'): '>', ('right', 'v'): '<', ('right', '>'): 'v', ('right', '<'): '^',
+}
+turn_sequence = ['left', 'straight', 'right']
+turn_symbols = ['/', '\\']
+
+# carts: {(0, 2): ('>', 'left'), (3, 9): ('v', 'left')}, ...}
+while True:
+    new_carts = {}
+    print(f'\nDealing with {carts}')
+    for position, (direction, next_turn) in carts.items():
+        new_position = tuple(map(operator.add, position, cart_directions[direction]))
+        print(f'{position}, {(direction, next_turn)} to {new_position}')
+        if new_position in new_carts.keys():
+            # collision
+            print(f'COLLISION {new_position}')
+            exit(0)
+        new_rail = rails[new_position]
+        if new_rail is '+':
+            # crossing
+            direction = turns[(next_turn, direction)]
+            print('    ', next_turn, turn_sequence.index(next_turn), (turn_sequence.index(next_turn)+1) % (len(turn_sequence)))
+            next_turn = turn_sequence[(turn_sequence.index(next_turn)+1) % (len(turn_sequence))]
+        if new_rail in turn_symbols:
+            # turn
+            direction = rail_directions[(new_rail, direction)]
+        print(f'  {new_position} rail is {new_rail} => {(direction, next_turn)}')
+        new_carts[new_position] = (direction, next_turn)
+
+    carts = new_carts
