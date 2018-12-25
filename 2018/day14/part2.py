7e53e2c8eaf7d3ef6bb26379ce5c3203574c225e
Youri Ackx <youri@ackx.net>
2018-12-25 19:01:04 +0100

    Add day14 part 2

---
+++ 2018/day14/part2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+def score(target):
+    recipes = '37'
+    elf1, elf2 = 0, 1
+    elf = lambda e: (e + 1 + int(recipes[e])) % len(recipes)
+
+    # The slice is mandatory for the computation to complete
+    # in a reasonable timeframe
+    while target not in recipes[-len(target)-1:]:
+        recipes += str(int(recipes[elf1]) + int(recipes[elf2]))
+        elf1, elf2 = elf(elf1), elf(elf2)
+
+    return recipes.index(target)
+
+
+assert score('51589') == 9
+assert score('01245') == 5
+assert score('92510') == 18
+assert score('59414') == 2018
+
+target = open('input.txt', 'r').read().strip()
+print(score(target))
