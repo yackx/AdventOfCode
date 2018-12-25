d32d6df3e9543ed558ffb04bbb3b122c9dfded55
Youri Ackx <youri@ackx.net>
2018-12-25 18:40:04 +0100

    Add day14 alternate solution (3rd party) (slower)

---
+++ 2018/day14/other.py
@@ -0,0 +1,15 @@
+# Part 1 is way slower than my solution
+# https://www.reddit.com/r/adventofcode/comments/a61ojp/2018_day_14_solutions/
+
+# recipes = open('day14.in','r').read().strip()
+recipes = '825401'  # puzzle input
+score = '37'
+elf1 = 0
+elf2 = 1
+while recipes not in score[-7:]:
+    score += str(int(score[elf1]) + int(score[elf2]))
+    elf1 = (elf1 + int(score[elf1]) + 1) % len(score)
+    elf2 = (elf2 + int(score[elf2]) + 1) % len(score)
+
+print('Part 1:', score[int(recipes):int(recipes)+10])
+print('Part 2:', score.index(recipes))
