ac3d30fae7f4066b112992cc930b621a4a763048
Youri Ackx <1193689+yackx@users.noreply.github.com>
2023-12-03 11:29:46 +0100

    Day 03 part 1

---
+++ 2023/aoc2023/src/main/kotlin/day03/part1/Part1.kt
@@ -0,0 +1,76 @@
+package day03.part1
+
+fun parseLine(row: Int, line: String): Schematic {
+    val isPeriod: (Char) -> Boolean = { it == '.' }
+    val isDigit: (Char) -> Boolean = { it.isDigit() }
+    val isSymbol: (Char) -> Boolean = { !isPeriod(it) && !isDigit(it) }
+
+    val engineParts = mutableListOf<EnginePart>()
+    val symbols = mutableListOf<Position>()
+    var state = ParserState.PERIOD
+    var part = 0
+    var col = 0
+
+    line.forEachIndexed { index, c ->
+        val previousState = state
+        state = when {
+            isPeriod(c) -> ParserState.PERIOD
+            isDigit(c) -> ParserState.PART
+            isSymbol(c) -> ParserState.SYMBOL
+            else -> throw IllegalArgumentException("Unexpected character: $c")
+        }
+        when (state) {
+            ParserState.PERIOD -> Unit
+            ParserState.PART -> {
+                if (previousState != ParserState.PART) {
+                    col = index  // New part
+                }
+                part = part * 10 + c.toString().toInt()
+            }
+            ParserState.SYMBOL -> symbols.add(Position(row, index))
+        }
+
+        if (previousState == ParserState.PART && (state != ParserState.PART || index == line.length - 1)) {
+            // Part is complete
+            engineParts.add(EnginePart(part, Position(row, col)))
+            part = 0
+        }
+
+    }
+    return Schematic(engineParts, symbols)
+}
+
+fun mergeSchematics(schematics: List<Schematic>): Schematic =
+    Schematic(schematics.flatMap { it.engineParts }, schematics.flatMap { it.symbols })
+
+fun solve(schematic: Schematic): Int {
+    fun neighbors(enginePart: EnginePart): List<Position> {
+        val (row, col) = enginePart.position
+        val length = enginePart.number.toString().length
+        val horizontalRange = IntRange(col-1, col+length)
+        return horizontalRange.map { Position(row-1, it) } +
+                horizontalRange.map { Position(row+1, it) } +
+                Position(row, col-1) +
+                Position(row, col+length)
+    }
+    val validParts = schematic.engineParts.filter {
+        neighbors(it).any { neighbor -> schematic.symbols.contains(neighbor) }
+    }
+    return validParts.sumOf { it.number }
+}
+
+fun main() {
+    val lines = generateSequence { readlnOrNull() }
+    val schematicLines = lines.mapIndexed { index, line -> parseLine(index, line) }
+    val schematic = mergeSchematics(schematicLines.toList())
+    val result = solve(schematic)
+    println(result)
+}
+
+data class Position(val row: Int, val col: Int)
+
+data class EnginePart(val number: Int, val position: Position)
+
+data class Schematic(val engineParts: List<EnginePart>, val symbols: List<Position>)
+
+enum class ParserState { PART, SYMBOL, PERIOD }
---
+++ 2023/aoc2023/src/test/kotlin/day03/TestPart1.kt
@@ -0,0 +1,89 @@
+package day03
+
+import day03.part1.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TestPart1 {
+
+    @Test
+    fun testParseLineEnginePartsOnly() {
+        val line = "467..114.."
+        val expected = Schematic(
+            listOf(
+                EnginePart(467, Position(1, 0)),
+                EnginePart(114, Position(1, 5)),
+            ),
+            emptyList()
+        )
+        val actual = parseLine(1, line)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testParseLineSymbolOnly() {
+        val line = "...*......"
+        val expected = Schematic(
+            emptyList(),
+            listOf(Position(5, 3))
+        )
+        val actual = parseLine(5, line)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testParseLineMixed() {
+        val line = "617*......"
+        val expected = Schematic(
+            listOf(
+                EnginePart(617, Position(3, 0)),
+            ),
+            listOf(Position(3, 3))
+        )
+        val actual = parseLine(3, line)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testParseSymbolPartAlone() {
+        val line = "114"
+        val expected = Schematic(
+            listOf(
+                EnginePart(114, Position(1, 0)),
+            ),
+            emptyList()
+        )
+        val actual = parseLine(1, line)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testSolveSample() {
+        val input = """
+            | 467..114..
+            |...*......
+            |..35..633.
+            |......#...
+            |617*......
+            |.....+.58.
+            |..592.....
+            |......755.
+            |...${'$'}.*....
+            |.664.598..""".trimIndent()
+        val schematicLines = input.lineSequence().mapIndexed { row, line -> parseLine(row, line) }
+        val schematic = mergeSchematics(schematicLines.toList())
+        val actual = solve(schematic)
+        val expected = 4361
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testSolveSymbolAdjacentLeft() {
+        val input = "#114"
+        val schematicLines = input.lineSequence().mapIndexed { row, line -> parseLine(row, line) }
+        val schematic = mergeSchematics(schematicLines.toList())
+        val actual = solve(schematic)
+        val expected = 114
+        assertEquals(expected, actual)
+    }
+}
