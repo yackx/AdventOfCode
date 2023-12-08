6bd7b124f4e16f62831d24135f9d7a68599b70b9
Youri Ackx <1193689+yackx@users.noreply.github.com>
2023-12-08 11:22:32 +0100

    Day 07 part 1

--- 2023/aoc2023/src/main/kotlin/day07/part1/Part1.kt
+++ 2023/aoc2023/src/main/kotlin/day07/part1/Part1.kt
@@ -4,20 +4,29 @@ typealias Hand = String
 typealias Bid = Int
 typealias Game = Pair<Hand, Bid>
 
-val cardValues = mapOf(
-    'A' to 14,
-    'K' to 13,
-    'Q' to 12,
-    'J' to 11,
-    'T' to 10,
-).plus((2..9).associateBy { it.toString()[0] })
-
-val gameComparator = compareBy<Game> { g -> g.first.toSet().size }
-    .thenBy { g ->
-        g.first
-            .map { cardValues[it]!! }
-            .reduce { acc, i -> acc * 16 + i }
+val strength: (Game) -> Comparable<*>? = { g ->
+    val cardsCount = g.first.groupingBy { it }.eachCount()
+    when (cardsCount.values.sortedDescending()) {
+        listOf(5) -> 7  // Five of a kind
+        listOf(4, 1) -> 6  // Four of a kind
+        listOf(3, 2) -> 5  // Full house
+        listOf(3, 1, 1) -> 4  // Three of a kind
+        listOf(2, 2, 1) -> 3  // Two pairs
+        listOf(2, 1, 1, 1) -> 2  // One pair
+        listOf(1, 1, 1, 1, 1) -> 1  // High card
+        else -> throw AssertionError("Invalid hand: ${g.first}")
     }
+}
+
+const val ranks = "23456789TJQKA"
+
+val tieBreaker: (Game) -> Comparable<*>? = { g ->
+    g.first
+        .map { h -> ranks.indexOf(h).toString(16) }
+        .joinToString("")
+}
+
+val gameComparator = compareBy(strength).thenBy(tieBreaker)
 
 fun solve(games: List<Game>): Int =
     games
